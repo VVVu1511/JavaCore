887691c4ac7b305063768f3fc8a31cd58bc51a4c
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-03-17 19:05:25 +0700

    DB

---
+++ All/Sec22/MySQLMusic/Main.java
@@ -0,0 +1,36 @@
+package Sec22.MySQLMusic;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import java.util.Arrays;
+
+
+public class Main {
+    
+    private final static String CONN_STRING = "jdbc:sqlserver://localhost:1433;databaseName=music";
+    
+    public static void main(String[] args) {
+        String username = JOptionPane.showInputDialog(
+            null,"Enter DB Username"
+        );
+
+        JPasswordField pf = new JPasswordField();
+        int okCxl = JOptionPane.showConfirmDialog(null, pf, "Enter DB Password", JOptionPane.OK_CANCEL_OPTION);
+
+        final char[] password = 
+            (okCxl == JOptionPane.OK_OPTION) ? pf.getPassword() : null;
+    
+        try(Connection connection = DriverManager.getConnection(
+            CONN_STRING, username, String.valueOf(password))){
+                System.out.println("Successfully connected to music DB!");
+                Arrays.fill(password,' ');
+            } catch(SQLException e){
+                throw new RuntimeException(e);
+            }
+        
+    }
+}
