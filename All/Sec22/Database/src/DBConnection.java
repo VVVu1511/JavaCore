23b83dea2536f1d40141f78db2663f4fcad44d33
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-03-21 21:26:00 +0700

    test db

--- All/Sec22/Database/src/DBConnection.java
+++ All/Sec22/Database/src/DBConnection.java
@@ -1,27 +1,30 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class DBConnection {
     static final String query1 = "select * from NHANVIEN";
-    static final String query2 = "update NHANVIEN set LUONG = 100000 where TEN = 'Phong'";
-    static final String query3 = "delete from NHANVIEN where MANV = 15";
+    static final String query2 = "update NHANVIEN set LUONG = 100000 where TEN = 'Quang'";
+    static final String query3 = "delete from NHANVIEN where MAN = 1001";
     static final String query4 = """
-                                    select cb1.MACB, cb1.GIODI, cb1.GIODEN
-                                    from CHUYENBAY cb1
-                                    where cb1.GIODI <= all(
-                                                select cb2.GIODI
-                                    from CHUYENBAY cb2
-                                )
+                                    select len(MACB) as length_code
+                                    from LICHBAY
                                 """;
+    static final String query5 = "update NHANVIEN set LUONG = 200000 where TEN = 'Phong'";
+    static final String query6 = "insert into LICHBAY values ({d '2005-11-15'}, '100',11,'B727')";
 
-    public static void run(String URL) {
-        try (Connection connection = DriverManager.getConnection(URL)) {
+    public static void run(String URL) throws SQLException {
+        Connection connection = DriverManager.getConnection(URL);
+        connection.setAutoCommit(false);
+        Savepoint savepoint1 = connection.setSavepoint("savepoint1");
+
+        try {
             Statement statement = connection.createStatement();
-//            statement.executeUpdate(query3);
+//            statement.addBatch(query2);
+//            statement.addBatch(query5);
+//            statement.addBatch(query6);
+            //            statement.addBatch(query1);
+
+//            statement.executeBatch();
 
             ResultSet resultSet = statement.executeQuery(query4);
             int columnCount = resultSet.getMetaData().getColumnCount();
@@ -33,9 +36,11 @@ public class DBConnection {
                 System.out.println();
             }
 
+            connection.commit();
 
         } catch (SQLException e) {
             e.printStackTrace();
+            connection.rollback(savepoint1);
             System.exit(1);
         }
     }
--- All/Sec22/Database/src/Main.java
+++ All/Sec22/Database/src/Main.java
@@ -1,9 +1,13 @@
-
+import java.sql.SQLException;
 
 public class Main {
     private final static String URL = "jdbc:sqlserver://DESKTOP-0E72VR4:1433;databaseName=QLyChuyenBay;integratedSecurity=true;encrypt=false";
 
     public static void main(String[] args) {
-        DBConnection.run(URL);
+        try {
+            DBConnection.run(URL);
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
