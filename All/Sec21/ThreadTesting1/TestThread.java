2347e33369a9d9b28cf5551dad4536e420d30b53
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-03-22 15:46:21 +0700

    lock

--- All/Sec21/ThreadTesting1/TestThread.java
+++ All/Sec21/ThreadTesting1/TestThread.java
@@ -1,27 +1,90 @@
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.Executors;
 
+
 public class TestThread{
+    private final ReentrantLock lock = new ReentrantLock(true);
+
+    public void operation(int n){
+        if(lock.tryLock()){
+            try{
+                lock.lock();
+                for(int i = 1; i <= 4; i++){
+                    System.out.println(n++);
+                }
+
+            } finally{
+                lock.unlock();
+            } 
+        }
+        else{
+            System.out.println("I am in else block");
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        final ScheduledExecutorService scheduler = 
-            Executors.newScheduledThreadPool(1);
+        ReentrantLock lock = new ReentrantLock();
+        TestThread tt = new TestThread();
 
-        final ScheduledFuture<?> beepHandler = 
-        scheduler.scheduleAtFixedRate(new BeepTask(), 1, 1, TimeUnit.SECONDS);
-        
-        final ScheduledExecutorService boss = 
-        Executors.newScheduledThreadPool(1);
-
-        boss.schedule(new Runnable() {
+        Thread thread1 = new Thread(new Runnable(){
             @Override
-            public void run(){
-                beepHandler.cancel(true);
-                scheduler.shutdown();
-                System.exit(1);  
+            public void run() {
+                tt.operation(1);
             }
-        }, 10, TimeUnit.SECONDS);
+        });
+
+        Thread thread2 = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                tt.operation(5);
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                tt.operation(10);
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        // final ScheduledExecutorService scheduler = 
+        //     Executors.newScheduledThreadPool(1);
+
+        // final ScheduledFuture<?> beepHandler = 
+        // scheduler.scheduleAtFixedRate(new BeepTask(), 1, 1, TimeUnit.SECONDS);
+        
+        // final ScheduledExecutorService boss = 
+        // Executors.newScheduledThreadPool(1);
+
+        // scheduler.schedule(new Runnable(){
+        //     @Override 
+        //     public void run(){
+        //         while(!lock){
+        //             scheduler.wait();
+        //         }
+        //         lock = false;
+        //         for(int i = 0; i < 100; i++) System.out.println("Yeu Linh Dan ghe");
+        //     }
+        // }, 1, TimeUnit.SECONDS);
+        // boss.schedule(new Runnable() {
+        //     @Override
+        //     public void run(){
+        //         while(!lock){
+        //             for(int i = 0; i < 100; i++) System.out.println("Yeu Dan Linh ghe");
+        //         }
+        //         lock = false;
+        //         beepHandler.cancel(true);
+        //         scheduler.shutdown();
+        //         System.exit(1);  
+        //     }
+        // }, 10, TimeUnit.SECONDS);
 
         // scheduler.schedule(new Runnable(){
         //     @Override 
